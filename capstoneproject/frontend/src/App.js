53e5ab8680b204b177afe5f943936fcd71d14fbb
Sidequest040 <167496229+Sidequest040@users.noreply.github.com>
2024-09-03 04:52:23 +0000

    Worked on making the program take the data and create a live graph of the program, will add more pages tmw and also will make is so that we no longer use mockdata but some real user data thats all - Eric Del Angel

--- capstoneproject/frontend/src/App.js
+++ capstoneproject/frontend/src/App.js
@@ -3,18 +3,24 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
+import ThreatDetectionPage from './components/ThreatDetectionPage';
+import ThreatDetection from './components/ThreatDetection';
+import { ThreatProvider } from './components/ThreatContext';  // Import the ThreatProvider
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                {/* Optionally, add a redirect from '/' to '/login' */}
-                <Route path="/" element={<Login />} />
-            </Routes>
-        </Router>
+        <ThreatProvider>  {/* Wrap your app in the provider */}
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/threat-detection-page" element={<ThreatDetectionPage />} />  {/* Add new route */}
+                    <Route path="/threat-detection" element={<ThreatDetection />} />  {/* Add new route */}
+                    <Route path="/" element={<Login />} />
+                </Routes>
+            </Router>
+        </ThreatProvider>
     );
 }
 
