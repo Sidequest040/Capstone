f2e714038308ff8951757e54c1370398bd95af41
Sidequest040 <167496229+Sidequest040@users.noreply.github.com>
2024-11-01 18:17:39 +0000

    Updating the codespace so its not deleted - Eric

--- capstoneproject/AdwareCacheClearerExtension (4)/AdwareCacheClearerExtension/popup/popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('clearButton').addEventListener('click', () => {
-    const timeRange = document.getElementById('timeRange').value;
-    chrome.runtime.sendMessage({ action: 'clearData', timeRange: parseInt(timeRange) });
-  });
-  
